3c23b29f03c36e4b3329c972ea34caf2262172bc
Shawn Anderson <shawn@longtailfinancial.com>
2023-09-04 15:22:03 -0700

    Init tegr1 app.

--- app/tegr1/__init__.py
+++ app/tegr1/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 import hvplot.pandas
 import numpy as np
 import pandas as pd
@@ -5,23 +7,29 @@ import panel as pn
 import param as pm
 from icecream import ic
 
-import logging
+
 def exception_handler(ex):
-    ic("🔥🔥🔥")
-    logging.error("Error", exc_info=ex)
-    pn.state.notifications.send('Error: %s' % ex, duration=int(10e3), background='black')
+    ic('🔥🔥🔥')
+    logging.error('Error', exc_info=ex)
+    pn.state.notifications.send(
+        'Error: %s' % ex, duration=int(10e3), background='black'
+    )
+
+
 pn.extension(exception_handler=exception_handler, notifications=True)
+pn.extension('tabulator')
 pn.state.notifications.position = 'top-right'
 
 
-class Donations(pm.Parameterized):
+class Dataset(pm.Parameterized):
     file = pm.FileSelector(
-        default='./app/input/vote_coefficients_input.csv',
+        default=None,
         path='./app/input/*.csv',
-        precedence=0.5,
     )
-    page_size = pm.Integer(default=20)
-    dataset = pm.DataFrame(default=None, columns={'voter', 'amountUSD'}, label="Donations Dataset")
+    page_size = pm.Integer(default=20, precedence=-1)
+    dataset = pm.DataFrame(
+        default=None,
+    )
 
     def __init__(self, **params):
         super().__init__(**params)
@@ -33,25 +41,71 @@ class Donations(pm.Parameterized):
 
     @pm.depends('dataset', 'page_size')
     def view(self):
-        view = pn.Param(
-            self.param,
-            sort=False,
-            widgets={
-                'dataset': {
-                    'widget_type': pn.widgets.Tabulator,
-                    'layout': 'fit_columns',
-                    'page_size': self.page_size,
-                    'pagination': 'remote',
-                    'header_filters': True,
+        view = pn.panel(
+            pn.Param(
+                self.param,
+                sort=False,
+                widgets={
+                    'dataset': {
+                        'widget_type': pn.widgets.Tabulator,
+                        'layout': 'fit_columns',
+                        'page_size': self.page_size,
+                        'pagination': 'remote',
+                        'header_filters': True,
+                    },
                 },
-            },
+            )
         )
         return view
 
 
+class Donations(Dataset):
+    file = pm.FileSelector(
+        default='./app/input/vote_coefficients_input.csv',
+        path='./app/input/*.csv',
+    )
+    dataset = pm.DataFrame(
+        default=None, columns={'voter', 'amountUSD'}, label='Donations Dataset'
+    )
+
+
+class TECTokenDistribution(Dataset):
+    file = pm.FileSelector(
+        default='./app/input/tec_holders.csv',
+        path='./app/input/*.csv',
+    )
+    dataset = pm.DataFrame(
+        default=None, columns={'address', 'balance'}, label='Token Dataset'
+    )
+
+
+class TEATokenDistribution(Dataset):
+    file = pm.FileSelector(
+        default='./app/input/tea_holders_dune.csv',
+        path='./app/input/*.csv',
+    )
+    dataset = pm.DataFrame(
+        default=None, columns={'address', 'balance'}, label='Token Dataset'
+    )
+
+    @pm.depends('file', watch=True)
+    def load_file(self):
+        df = pd.read_csv(self.file)
+        df.rename({'wallet': 'address'}, axis=1, inplace=True)
+        self.dataset = df
+
+
 donations = Donations()
 
-app = pn.Column(donations.view)
+tec_distribution = TECTokenDistribution()
+
+tea_distribution = TEATokenDistribution()
+
+app = pn.Tabs(
+    ('Donations', donations.view()),
+    ('Token Distribution', tec_distribution.view()),
+    ('TEA Token Distribution', tea_distribution.view()),
+)
 
 if __name__ == '__main__':
     print(donations)
