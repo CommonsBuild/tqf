e229f4263e33a487c1436245afec996aeddfcfe1
Shawn Anderson <shawn@longtailfinancial.com>
2023-12-01 01:04:13 -0800

    Refactoring boost factory. Close to efficiency.

--- app/tqf/boost_factory.py
+++ app/tqf/boost_factory.py
@@ -1,55 +1,89 @@
+import holoviews as hv
+import numpy as np
 import pandas as pd
 import panel as pn
 import param as pm
+
+hv.opts.defaults(
+    hv.opts.Curve(axiswise=True, framewise=True, shared_axes=False),
+    hv.opts.Scatter(axiswise=True, framewise=True, shared_axes=False),
+    hv.opts.Image(axiswise=True, framewise=True, shared_axes=False),
+    hv.opts.Histogram(axiswise=True, framewise=True, shared_axes=False),
+)
 
 from .boost import Boost
 
 
 class BoostFactory(pm.Parameterized):
-    boosts = pm.ListSelector(default=[], precedence=1)
-    combine_method = pm.Selector(default='max', objects=['max', 'sum'])
+    boosts = pm.ListSelector(default=[], precedence=-1)
+    combine_method = pm.Selector(default='max', objects=['max', 'sum', 'product'])
     boost_outputs = pm.DataFrame(precedence=1)
 
-    @pm.depends('combine_method', 'boosts', watch=True, on_init=True)
+    # def _on_boost_change(self, event):
+    #     self.param.trigger('combine_method')
+    #
+    # @pm.depends('boosts', watch=True, on_init=True)
+    # def _update_watchers(self):
+    #     for boost in self.boosts:
+    #         boost.param.watch(self._on_boost_change, 'max_boost')
+
+    @pm.depends('combine_method', watch=True, on_init=True)
     def update_boost_outputs(self):
         boost_outputs_list = [boost.distribution.dataset for boost in self.boosts]
 
-        # No boosts. Return empty dataframe
-        if not boost_outputs_list:
-            boost_outputs = pd.DataFrame(columns=['address', 'balance', 'boost'])
-
         # Merge Boosts
-        else:
+        if boost_outputs_list:
             boost_outputs = boost_outputs_list[0]
             for idx, df in enumerate(boost_outputs_list[1:], start=1):
                 boost_outputs = boost_outputs.merge(
                     df, on='address', how='outer', suffixes=('', f'_{idx}')
                 )
 
-        # Generate the total boost column depending on combination method
-        boost_outputs = boost_outputs.fillna(0)
-        if self.combine_method == 'max':
-            boost_outputs['total_boost'] = boost_outputs[
-                [col for col in boost_outputs.columns if 'boost' in col]
-            ].max(axis=1)
-        elif self.combine_method == 'sum':
-            boost_outputs['total_boost'] = boost_outputs[
-                [col for col in boost_outputs.columns if 'boost' in col]
-            ].sum(axis=1)
+        # No boosts. Start with empty dataframe
+        else:
+            boost_outputs = pd.DataFrame(columns=['address', 'balance', 'boost'])
 
         # Add suffix to boost 0
-        boost_outputs = boost_outputs.rename(
-            {'balance': 'balance_0', 'boost': 'boost_0'}, axis=1
+        boost_outputs.rename(
+            {'balance': 'balance_0', 'boost': 'boost_0'}, axis=1, inplace=True
         )
 
-        # Add Sort primarly by Total Boost and Secondarly by balance 0
+        # Replace nan with 0
+        boost_outputs = boost_outputs.fillna(0)
+
+        boost_columns = [c for c in boost_outputs.columns if c.startswith('boost')]
+
+        # Select the max boost
+        boost_outputs['max'] = boost_outputs[boost_columns].max(axis=1)
+
+        # Sum boosts
+        boost_outputs['sum'] = boost_outputs[boost_columns].sum(axis=1)
+
+        # Indicate if any boosts have been achieved
+        boost_outputs['any'] = (
+            (boost_outputs[boost_columns] >= 1).any(axis=1).astype(int)
+        )
+
+        # Take the product of boosts in such a way that if any boost is achieved then minimum product is 1, 0 otherwise
+        boost_outputs['product'] = np.where(
+            boost_outputs['any'],
+            boost_outputs[boost_columns].clip(lower=1).prod(axis=1),
+            0,
+        )
+
+        # Set the total boost column as the combine method
+        boost_outputs['total_boost'] = boost_outputs[self.combine_method]
+
+        # Sort by Total Boost and secondly by balance_0
         boost_outputs = boost_outputs.sort_values(
             ['total_boost', 'balance_0'], ascending=False
         )
+
+        # Set boost_outputs dataframe on class
         self.boost_outputs = boost_outputs
 
     @pm.depends('boost_outputs')
-    def view_boost_outputs(self):
+    def view_boost_outputs_table(self):
         boost_outputs_view = pn.panel(
             pn.Param(
                 self.param['boost_outputs'],
@@ -69,25 +103,40 @@ class BoostFactory(pm.Parameterized):
         return boost_outputs_view
 
     @pm.depends('boost_outputs')
-    def view_boosts(self):
+    def view_boost_outputs_chart(self):
         if self.combine_method == 'max':
             stacked = False
+            y = [c for c in self.boost_outputs.columns if c.startswith('boost')]
         elif self.combine_method == 'sum':
             stacked = True
+            y = [c for c in self.boost_outputs.columns if c.startswith('boost')]
+
+        elif self.combine_method == 'product':
+            stacked = False
+            y = 'product'
 
         boosts_view = self.boost_outputs.reset_index(drop=True).hvplot.area(
             x='index',
-            y=[c for c in self.boost_outputs.columns if c.startswith('boost')],
+            y=y,
             stacked=stacked,
             height=400,
             responsive=True,
+            shared_axes=False,
         )
         return boosts_view
 
+    def view_boosts_list_param(self):
+        return pn.panel(self.param['boosts'], expand_button=False)
+
+    def view_boosts(self):
+        return pn.Column(*[boost.view() for boost in self.boosts])
+
+    @pm.depends('boost_outputs')
     def view(self):
         return pn.Column(
-            pn.panel(self.param['boosts'], expand_button=False),
-            self.param['combine_method'],
-            self.view_boost_outputs(),
+            self.view_boosts_list_param,
             self.view_boosts,
+            self.param['combine_method'],
+            self.view_boost_outputs_table,
+            self.view_boost_outputs_chart,
         )
